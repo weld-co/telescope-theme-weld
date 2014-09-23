ab734cfb95e74953c15e5d7ccfa538bb5d1b823a
Jamie Wilson <jamie@jamiewilson.io>
2014-09-22 19:04:33 -0500

    lots of updates, mainly mobile nav though

--- package.js
+++ package.js
@@ -1,12 +1,18 @@
-Package.describe({summary: "Telescope WELD theme"});
+Package.describe({ 
+    summary: "Telescope WELD Theme Package",
+    version: '0.1.0',
+    name: "telescope-theme-weld"
+});
 
 Package.on_use(function (api) {
 
   api.use([
     'templating',
     'telescope-base',
+    'telescope-lib',
     'telescope-tags',
     'telescope-search',
+    'telescope-notifications',
     'telescope-newsletter'
   ], ['client']);
 
@@ -34,14 +40,20 @@ Package.on_use(function (api) {
     'lib/client/templates/post_domain_weld.html',
     'lib/client/templates/post_title_weld.html',
     'lib/client/templates/post_categories_weld.html',
+    'lib/client/templates/user_item_weld.html',
+    'lib/client/templates/no_rights_weld.html',
+    'lib/client/templates/not_found_weld.html',
 
     'lib/client/js/weld.js',
     'lib/client/js/base_weld.js',
+    'lib/client/js/mobile_nav_weld.js',
     'lib/client/js/notifications_menu_weld.js',
     'lib/client/js/post_info_weld.js',  
     'lib/client/js/user_profile_weld.js',
     'lib/client/js/post_domain_weld.js',
     'lib/client/js/sidebar_weld.js',
+    'lib/client/js/progress_bar.js',
+
 
     'img/apple-touch-120.png',
     'img/apple-touch-152.png',
