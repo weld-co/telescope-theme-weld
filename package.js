df4a1d6369009180cb3e69a8fc0bdb61bdd79aba
Jamie Wilson <jamie@jamiewilson.io>
2014-09-24 11:55:21 -0500

    testing overrides, changing name of meta info to post_info

--- package.js
+++ package.js
@@ -8,45 +8,45 @@ Package.on_use(function (api) {
 
   api.use([
     'templating',
-    'telescope-base',
-    'telescope-lib',
     'telescope-tags',
     'telescope-search',
     'telescope-notifications',
-    'telescope-newsletter'
-  ], ['client']);
+    'telescope-newsletter',
+    'telescope-lib', 
+    'telescope-base',
+    'iron:router'
+  ], ['client', 'server']);
 
   api.add_files([
     'lib/client/stylesheets/main.css',
 
     'lib/client/templates/categories_menu_weld.html',
     'lib/client/templates/comment_item_weld.html',
+    'lib/client/templates/footer_weld.html',
     'lib/client/templates/layout_weld.html',
-    'lib/client/templates/main_weld.html',
     'lib/client/templates/mobile_nav_weld.html',
     'lib/client/templates/nav_weld.html',
     'lib/client/templates/newsletter_banner_weld.html',
     'lib/client/templates/notifications_menu_weld.html',
+    'lib/client/templates/post_categories_weld.html',
+    'lib/client/templates/post_domain_weld.html',
     'lib/client/templates/post_info_weld.html',
     'lib/client/templates/post_item_weld.html',
     'lib/client/templates/post_submit_weld.html',
+    'lib/client/templates/post_title_weld.html',
     'lib/client/templates/posts_list_weld.html',
     'lib/client/templates/search_weld.html',
     'lib/client/templates/submit_button_weld.html',
+    'lib/client/templates/user_edit_weld.html',
+    'lib/client/templates/user_item_weld.html',
     'lib/client/templates/user_menu_weld.html',
     'lib/client/templates/user_profile_weld.html',
     'lib/client/templates/views_menu_weld.html',
-    'lib/client/templates/user_edit_weld.html',
-    'lib/client/templates/post_domain_weld.html',
-    'lib/client/templates/post_title_weld.html',
-    'lib/client/templates/post_categories_weld.html',
-    'lib/client/templates/user_item_weld.html',
-    'lib/client/templates/no_rights_weld.html',
-    'lib/client/templates/not_found_weld.html',
 
     'lib/client/js/weld.js',
     'lib/client/js/base_weld.js',
     'lib/client/js/mobile_nav_weld.js',
+    'lib/client/js/comment_item_weld.js',
     'lib/client/js/notifications_menu_weld.js',
     'lib/client/js/post_info_weld.js',  
     'lib/client/js/user_profile_weld.js',
