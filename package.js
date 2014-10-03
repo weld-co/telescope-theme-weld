ec2d1f5c0c2bdd8de1ac03dc6430d8930fcc14c4
Jamie Wilson <jamie@jamiewilson.io>
2014-10-03 16:33:00 -0500

    lots and lots of changes

--- package.js
+++ package.js
@@ -8,7 +8,6 @@ Package.on_use(function (api) {
 
   api.use([
     'templating',
-    'telescope-base',
     'telescope-tags',
     'telescope-search',
     'telescope-module-embedly',
@@ -16,22 +15,34 @@ Package.on_use(function (api) {
     'telescope-newsletter'
   ], ['client']);
 
+  api.use([
+    'telescope-lib', 
+    'telescope-base',
+    'iron:router'
+  ], ['client', 'server']);
+
   api.add_files([
     'lib/client/stylesheets/main.css',
 
     'lib/client/templates/categories_menu_weld.html',
     'lib/client/templates/comment_item_weld.html',
+    'lib/client/templates/error_item_weld.html',
     'lib/client/templates/footer_weld.html',
     'lib/client/templates/layout_weld.html',
     'lib/client/templates/mobile_nav_weld.html',
     'lib/client/templates/nav_weld.html',
+    'lib/client/templates/newsletter_banner_weld.html',
+    'lib/client/templates/no_invite_weld.html',
+    'lib/client/templates/no_rights_weld.html',
+    'lib/client/templates/not_found_weld.html',
     'lib/client/templates/notifications_menu_weld.html',
     'lib/client/templates/post_domain_weld.html',
+    'lib/client/templates/post_edit_weld.html',
     'lib/client/templates/post_info_weld.html',
     'lib/client/templates/post_item_weld.html',
     'lib/client/templates/post_submit_weld.html',
+    'lib/client/templates/post_thumbnail_weld.html',
     'lib/client/templates/post_title_weld.html',
-    'lib/client/templates/post_edit_weld.html',
     'lib/client/templates/posts_list_weld.html',
     'lib/client/templates/search_weld.html',
     'lib/client/templates/submit_button_weld.html',
@@ -40,8 +51,15 @@ Package.on_use(function (api) {
     'lib/client/templates/user_menu_weld.html',
     'lib/client/templates/user_profile_weld.html',
     'lib/client/templates/views_menu_weld.html',
-    'lib/client/templates/post_thumbnail_weld.html',
 
+    'lib/client/templates/members_page.html',
+    'lib/client/templates/members_item.html',
+    'lib/client/js/members_page.js',
+    'lib/client/js/members_item.js',
+    'lib/client/js/members.js',
+
+    'lib/client/js/fastclick.js',
+    'lib/client/js/fastclick_init.js',
     'lib/client/js/weld.js',
     'lib/client/js/base_weld.js',
     'lib/client/js/mobile_nav_weld.js',
@@ -53,6 +71,7 @@ Package.on_use(function (api) {
     'lib/client/js/sidebar_weld.js',
     'lib/client/js/post_thumbnail_weld.js',
     'lib/client/js/progress_bar.js',
+    'lib/client/js/newsletter_banner_weld.js',
 
     'img/apple-touch-icon-120.png',
     'img/apple-touch-icon-180.png',
@@ -65,6 +84,11 @@ Package.on_use(function (api) {
     'img/weld_logo.png'
 
   ], ['client']);
+
+  api.add_files([
+    'lib/server/templates/emailDigestWeld.handlebars',
+    'lib/server/js/email_templates_weld.js'
+  ], ['server']);
 
   api.export([
     'viewNav',
