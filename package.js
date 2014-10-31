20d81e31b562e7cadf937ae0573afd5ba62aed44
Jamie Wilson <jamie@jamiewilson.io>
2014-10-31 15:06:48 -0500

    sidebar additions, markdown cheat sheet

--- package.js
+++ package.js
@@ -47,7 +47,12 @@ Package.on_use(function (api) {
     'lib/client/templates/user_profile_weld.html',
     'lib/client/templates/views_menu_weld.html',
     'lib/client/templates/post_page_weld.html',
+    'lib/client/templates/members_menu_weld.html',
+    'lib/client/templates/comment_form_weld.html',
+    'lib/client/templates/cheatsheet.html',
 
+    'lib/client/js/fastclick_init.js',
+    'lib/client/js/fastclick.js',
     'lib/client/js/weld.js',
     'lib/client/js/base_weld.js',
     'lib/client/js/mobile_nav_weld.js',
@@ -60,9 +65,10 @@ Package.on_use(function (api) {
     'lib/client/js/progress_bar.js',
     'lib/client/js/newsletter_banner_weld.js',
     'lib/client/js/avatar_weld.js',
-    'lib/client/js/post_page_weld.js',
     'lib/client/js/post_item_weld.js',
     'lib/client/js/post_title_weld.js',
+    'lib/client/js/post_submit_weld.js',
+
 
     'img/apple-touch-icon-120.png',
     'img/apple-touch-icon-180.png',
